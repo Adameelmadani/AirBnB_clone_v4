ab28965872564e917529ee1271f2f62480900ca5
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-11-05 20:43:17 +0100

    Update 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,12 +9,23 @@ $('document').ready(function () {
     }
     $('.amenities H4').text(Object.values(amenities).join(', '));
   });
-});
+  const apiUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  // Function to update the status based on the API response
+  function updateApiStatus() {
+    $.get(apiUrl, function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }).fail(function () {
+      $('#api_status').removeClass('available');
+    });
+  }
 
-$.get("http://0.0.0.0:5001/api/v1/status/", function(r) {
-	if (r.status === "OK"){
-		$("div#api_status").addClass("available");
-	} else {
-		$("div#api_status").removeClass("available");
-	}
+  // Call the function to update the status initially
+  updateApiStatus();
+
+  // Set an interval to periodically update the status
+  setInterval(updateApiStatus, 5000); // Update every 5 seconds
 });
