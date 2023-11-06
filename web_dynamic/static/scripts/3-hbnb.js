04bf526bc58f687a768d22df8caa7f6d81e9d94f
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-11-06 01:26:19 +0100

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,11 +1,13 @@
 $('document').ready(function () {
   const api = 'http://' + window.location.hostname;
 
-  $.get('http://intellipow.tech:5001:/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $('DIV#api_status').addClass('available');
-    } else {
-      $('DIV#api_status').removeClass('available');
+  $.get('http://intellipow.tech:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
     }
   });
 
